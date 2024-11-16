115864d0c2f4f09fddd730d5b0db6dbc9731b3b7
arkadiuszsuchenia <arkadiusz.suchenia@govimana.com>
2024-11-16 14:15:18 +0100

    Removed unused method to generate Alert as JSON

--- svc-service/src/main/java/pl/kielce/tu/svcservice/model/Alert.java
+++ svc-service/src/main/java/pl/kielce/tu/svcservice/model/Alert.java
@@ -1,10 +1,3 @@
 package pl.kielce.tu.svcservice.model;
 
-import com.google.gson.Gson;
-
-public record Alert(String message, String data) {
-
-    public static String toJSON(Alert alert) {
-        return new Gson().toJson(alert);
-    }
-}
+public record Alert(String message, String data) { }

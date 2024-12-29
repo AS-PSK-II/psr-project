00ef9f36d0f7ac8e2f016d55dc1f34072ecc2609
Arkadiusz Suchenia <suchenia.arkadiusz@aru-software.pl>
2024-12-29 14:10:01 +0100

    Read env variables and add docker image for svc-alerts

--- run-local-docker.sh
+++ run-local-docker.sh
@@ -12,8 +12,8 @@ docker build -t distributed-iot/collector:latest .
 #cd ../svc-devices
 #docker build -t distributed-iot/svc-devices:latest .
 
-#cd ../svc-alerts
-#docker build -t distributed-iot/svc-alerts:latest .
+cd ../svc-alerts
+docker build -t distributed-iot/svc-alerts:latest .
 
 cd ../
 docker-compose -f local-services-compose.yml up -d
